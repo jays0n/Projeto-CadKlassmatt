90829a2f80771f3753cb335f235a984e30c94463
jays0n <67333306+jays0n@users.noreply.github.com>
2021-06-24 19:03:43 -0300

    Update Setup.py

--- Module/Setup.py
+++ Module/Setup.py
@@ -1,13 +1,16 @@
 import sys
 from cx_Freeze import setup, Executable
 
-build_exe_options={"packages":["os","PySimpleGui","selenium","re","time","openpyxl"],"include_files":["config.dat","geckodriver.exe","chromedriver.exe"]}
+build_exe_options={
+    "packages":["os","PySimpleGui","selenium","re","time","openpyxl","win32com","json","PyQt5","random","tempfile"],
+    "include_files":[r"Module\__xbase__"]
+    }
 
 base=None
 if sys.platform=="win32":
     base="Win32GUI"
 
-setup(name="CadKlassmatt",version="0.1",description="Aplicativo para cadastro de itens no site do Klassmatt de forma automatica.",
+setup(name="CadKmt",version="0.1",description="Aplicativo para cadastro de itens no site do Klassmatt de forma automatica.",
       options={"build_exe":build_exe_options},executables=[Executable("CadKlassmatt.py",base=base)])
 
 
