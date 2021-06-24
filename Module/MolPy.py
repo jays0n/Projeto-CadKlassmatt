844f66c2bda6c6b77bd3d40316d2ce33934df140
jays0n <67333306+jays0n@users.noreply.github.com>
2021-06-24 18:36:49 -0300

    Update MolPy.py

--- Module/MolPy.py
+++ Module/MolPy.py
@@ -1,91 +1,147 @@
 import win32com as wcp
 import win32com.client as clt
 import ProgPy as pp
+from PyDefines import *
 
-ErrosOutlook={'OlEmailNaoEncontrado':0,'OlEmailOk':1}
+class pyEmail:
+    def __init__(self,codSol=STRNULL,sendAuto=FALSE,messageMail=STRNULL):
+        self.outlook=clt.Dispatch("Outlook.Application").GetNamespace("MAPI")
+        self.accounts= clt.Dispatch("Outlook.Application").Session.Accounts
+        self.mainAcount=self.outlook.Folders(self.accounts[0].DeliveryStore.DisplayName)
+        self.kmtAcount=self.outlook.Folders["Cadastro Klassmatt"]
+        self.kmtInbox=self.kmtAcount.Folders["Inbox"]
+        try:
+            self.mainInbox=self.mainAcount.Folders['Inbox']   
+        except:
+            self.mainInbox=self.mainAcount.Folders['Caixa de Entrada']  
 
-def ResponderEmail(codSol,enviaAuto=False,messageMail=''):
-    outlook= clt.Dispatch("Outlook.Application").GetNamespace("MAPI")
-    accounts= clt.Dispatch("Outlook.Application").Session.Accounts
-    verificador=0
+        self.codSol=codSol.upper().strip()
+        self.sendAuto=sendAuto
+        self.messageMail=messageMail
 
-    pp.progress(120,['Conectando..'])
-    
-    #SETA CAIXA PRINCIPAL
-    mainAcount=outlook.Folders(accounts[0].DeliveryStore.DisplayName)
-    
-    kmtAcount=outlook.Folders["Cadastro Klassmatt"]
-    
-    kmtInbox=kmtAcount.Folders["Inbox"]
-    
-    mainInbox=mainAcount.Folders['Inbox']    
-    
-    #LOOP - KLASSMATT:
-    msg=kmtInbox.Items
-    tam=msg.count-1
-    i=tam
-    
-    message=None
-   
-    while i>tam-300:
+    def getEmail(self,code,withAttachment=FALSE,attachmentMustBeXls=FALSE):
+        emailsKmt=self.kmtInbox.items
+        lenghtKmt=emailsKmt.count-1
+        contKmt=lenghtKmt
+
+        emailsMain=self.mainInbox.items
+        lenghtMain=emailsMain.count-1
+        contMain=lenghtMain
+
+        emailItem=NULL
+        cont=0
+        try:
+            found=FALSE
+            while cont<500:
+                i=contKmt-cont
+                subject=str(emailsKmt[i].Subject).upper().strip()
+                if subject.find(self.codSol)>EOF:
+                    emailItem=emailsKmt[i]
+                    if withAttachment==TRUE:
+                        if emailItem.attachments.count>0:
+                            if attachmentMustBeXls==TRUE:                                
+                                for eachItem in emailItem.attachments:
+                                    if str(eachItem.FileName).find('xls')>EOF:
+                                        found=TRUE
+                                        break
+                                if found==TRUE:
+                                    break
+                            else:
+                                break
+                    else:                        
+                        break
+
+                i=contMain-cont
+                subject=str(emailsMain[i].Subject).upper().strip()
+                if subject.find(self.codSol)>EOF:
+                    emailItem=emailsKmt[i]
+                    if withAttachment==TRUE:
+                        if emailItem.attachments.count>0:
+                            if attachmentMustBeXls==TRUE:
+                                for eachItem in emailItem.attachments:
+                                    if str(eachItem.FileName).find('xls')>EOF:
+                                        found=TRUE
+                                        break
+                                if found==TRUE:
+                                    break
+
+                            else:
+                                break
+                    else:                        
+                        break
+                cont+=1
+        except Exception as msgErr:
+            return FALSE,ERR_EMAIL_NOT_FOUND,msgErr,NULL
+        if cont>=500 or emailItem==NULL:
+            return FALSE,ERR_EMAIL_NOT_FOUND,'Email não encontrado',NULL
         
-        if str(str(msg[i].Subject).upper()).find(str(str(codSol).strip()).upper())>-1:
-            message=msg[i]
-            break
-        i-=1
-        
+        return TRUE,ERR_NO_ERRO,STRNULL,emailItem
 
-    if message!=None:
-        responder=message.ReplyAll()
-        responder.BodyFormat=2  # olFormatHTM
-        responder.HTMLBody=messageMail+responder.HTMLBody
-        responder.To=str(str(responder.To).replace("cadastro.klassmatt@loginlogistica.com.br","",1)).replace("Cadastro Klassmatt","",1)
-        responder.CC=str(str(responder.CC).replace("cadastro.klassmatt@loginlogistica.com.br","",1)).replace("Cadastro Klassmatt","",1)
-        responder.CC= responder.CC + "; cadastro.klassmatt@loginlogistica.com.br"
-        
+    def saveAttachement(self):
+        response=self.getEmail(self.codSol,TRUE,TRUE)
+        if response[0]==FALSE:
+            return response
+        emailItem=response[3]
+        attachments=emailItem.attachments
 
-        if enviaAuto==True:
-            responder.Send()
-        else:
-            responder.Display()
+        if attachments.count<=0:
+            return FALSE,ERR_WITHOUT_ANY_ATTACHMENT,'Sem anexo',STRNULL
 
-        verificador=1
-
-    
-
-    if verificador==0:
-
-        #LOOP - MAIN INBOX:
-        msg=mainInbox.Items
-        tam=msg.count-1
-        i=tam   
-
-        message=None
-        while i>tam-300:
-            
-            if str(str(msg[i].Subject).upper()).find(str(codSol).upper())>-1:
-                message=msg[i]
+        for eachItem in attachments:
+            if str(eachItem.FileName).find('xls')>EOF:
                 break
-            i-=1
-            
+        path=PATH_STANDARD_TEMP_DIRECTORY+"\\"+eachItem.FileName
+        formatFile=path[-4::]
+        if formatFile=='.xls':
+            path=path+'x'
 
-        if message!=None:
-            responder=message.ReplyAll()
-            responder.BodyFormat=2  # olFormatHTM
-            responder.Body=messageMail+responder.Body
-            responder.To=str(str(responder.To).replace("cadastro.klassmatt@loginlogistica.com.br","",1)).replace("Cadastro Klassmatt","",1)
-            responder.CC=str(str(responder.CC).replace("cadastro.klassmatt@loginlogistica.com.br","",1)).replace("Cadastro Klassmatt","",1)
-            responder.CC="cadastro.klassmatt@loginlogistica.com.br"
-            
+        try:
+            eachItem.SaveAsFile(path)
+        except Exception as msgErr:
+            return FALSE,ERR_NO_PERMISSION_ACCESS_OUTLOOK,msgErr,STRNULL
+        return TRUE,ERR_NO_ERRO,STRNULL,path
 
-            if enviaAuto==True:
-                responder.Send()
+    def sendEmail(self):      
+
+        response=self.getEmail(self.codSol)
+        if response[0]==FALSE:
+            return response[0:3]
+
+        emailItem=response[3]
+
+        replyEmail=emailItem.ReplyAll()
+        replyEmail.BodyFormat=2
+        replyEmail.HTMLBody=self.messageMail+replyEmail.HTMLBody
+        replyEmail.To=str(str(replyEmail.To).replace("cadastro.klassmatt@loginlogistica.com.br","",1)).replace("Cadastro Klassmatt","",1)
+        replyEmail.CC=str(str(replyEmail.CC).replace("cadastro.klassmatt@loginlogistica.com.br","",1)).replace("Cadastro Klassmatt","",1)
+        replyEmail.CC= replyEmail.CC + "; cadastro.klassmatt@loginlogistica.com.br"
+
+        try:
+            if self.sendAuto==TRUE:
+                replyEmail.Send()
             else:
-                responder.Display()
+                replyEmail.Display()
+        except Exception as msgErr:
+            return FALSE,ERR_EMAIL_NOT_SEND,msgErr
 
-            verificador=1
+        return TRUE,ERR_NO_ERRO,STRNULL
 
-    if verificador==0:
-        return 0 #ErrosOutlook.pop('OlEmailNaoEncontrado')
-    else:
-        return 1 #ErrosOutlook.pop('OlEmailOk')
+    def __changeFromTextToHTML__(self,table):
+
+        tableText=r"<br /><p> <table border=5 CELLSPACING=2 CELLPADDING=6>"
+        tableText+="<tr>"
+        for eachField in table[0].keys():
+            tableText+="<td><b><font color='black'>"+str(eachField).upper()+"</font></b></td>"        
+
+        postion=0
+        for eachDct in table:
+            tableText+="<tr>"
+            for position in range(len(eachDct)):
+                tableText+="<td>" + str(list(eachDct.values())[position]).upper() + "</td>"
+            tableText+="</tr>"
+        tableText+="</table>"
+        return tableText
+
+    def setText(self,table):
+        self.messageMail+=self.__changeFromTextToHTML__(table)
+        self.messageMail+=EMAIL_SIGNATURE
